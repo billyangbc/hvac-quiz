6a0306343c1020994cea7b1d18aa5b6276102856
Bill Yang <bill.yang@live.ca>
2025-02-14 21:51:54 -0800

    frontend: added middleware for protected pages

---
+++ frontend/middleware.ts
@@ -0,0 +1,26 @@
+// frontend/middleware.ts
+import { withAuth } from "next-auth/middleware"
+import { NextResponse } from "next/server"
+
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req) {
+    console.log(req.nextauth.token);
+    console.log(req.nextUrl);
+    const { token } = req.nextauth;
+    const { pathname, origin} = req.nextUrl;
+
+    // Only admin have access to dashboard
+    if (pathname.startsWith("/dashboard") && token?.role !== "admin") {
+      return NextResponse.redirect(`${origin}/unauthorized`);
+    }
+  },
+  {
+    callbacks: {
+      // If `authorized` returns `true`, the middleware function will execute.
+      authorized: ({ token }) => token !== null
+    },
+  }
+)
+
+export const config = { matcher: ["/quiz/:path*", "/dashboard/:path*"] }
