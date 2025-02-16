69cc5df26af03e3446d29b4a75b742075688dc59
Bill Yang <bill.yang@live.ca>
2025-02-15 21:54:28 -0800

    frontend: added navigation menu

--- frontend/components/header/Navbar.tsx
+++ frontend/components/header/Navbar.tsx
@@ -1,13 +1,48 @@
 import Link from 'next/link';
 import NavbarUser from './NavbarUser';
+import { LuSquareCheck, LuChartColumn } from "react-icons/lu";
+
+const navItems = [
+  {
+    name: "Quiz",
+    icon: LuSquareCheck,
+    link: "/quiz",
+  },
+  {
+    name: "My Stats",
+    icon: LuChartColumn,
+    link: "/stats",
+  },
+];
 
 export default async function NavBar() {
-  return (
-    <nav className='flex gap-4 items-center my-4 p-4 bg-zinc-100 rounded-md'>
-      <Link href='/' className='mr-auto'>
-        ENZE Pro
+return (
+  <header className="min-h-[8vh] px-[2rem] bg-white shadow-lg flex items-center">
+    <nav className="flex-1 flex items-center justify-between">
+      <Link href='/' className='flex items-center gap-2'>
+        <h1 className="text-3xl font-bold text-primary/90">ENZE Pro</h1>
       </Link>
-      <NavbarUser />
+      <ul className="flex items-center gap-4">
+        {navItems.map((item, index) => (
+          <li key={index}>
+            <Link
+              href={item.link}
+              className={`py-1 px-6 flex items-center gap-2 text-lg leading-none text-primary/90 rounded-lg`}
+            >
+              <item.icon className="size-5" />
+              <span className={`font-bold uppercase text-primary/90`} >
+                {item.name}
+              </span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div className="flex items-center gap-2">
+        <NavbarUser />
+      </div>
     </nav>
-  );
+  </header>
+);
+
 }
+
