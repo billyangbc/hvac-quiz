cc151d6ecb382f40f8b34dc4902022c252b721d9
Bill Yang <bill.yang@live.ca>
2025-02-18 11:50:15 -0800

    frontend: fixed hook use issue

--- frontend/components/auth/logged-in-status/LoggedInClient.tsx
+++ frontend/components/auth/logged-in-status/LoggedInClient.tsx
@@ -3,11 +3,10 @@
 import { useSession } from 'next-auth/react';
 
 export default function LoggedInClient() {
-  // hide in production
-  if (process.env.NODE_ENV === "production") return null;
-
   const { data: session } = useSession();
   // console.log('useSession', session);
+  // hide in production
+  if (process.env.NODE_ENV === "production") return null;
   return (
     <div
       className={`p-4 basis-2/4 rounded-md text-center ${
