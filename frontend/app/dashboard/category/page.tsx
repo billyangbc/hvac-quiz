b9f3194b1de7ab8096d975b441cbc48fb9ec9dc9
Bill Yang <bill.yang@live.ca>
2025-02-27 20:57:42 -0800

    frontend: created dashboard pages as placeholder

---
+++ frontend/app/dashboard/category/page.tsx
@@ -0,0 +1,12 @@
+export default async function Index() {
+  return (
+    <div className="grid grid-cols-1 gap-4 p-4">
+      <div className="grid grid-cols-1">
+        create category component
+      </div>
+      <div className="grid grid-cols-1">
+        categories list 
+      </div>
+    </div>
+  );
+}
---
+++ frontend/app/dashboard/question/page.tsx
@@ -0,0 +1,8 @@
+export default async function Index() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+        <h1>questions page</h1>
+      </div>
+    );
+  }
+  
