4df9e977a633c803113e3a38a46832a6cc758405
Bill Yang <bill.yang@live.ca>
2025-03-05 21:22:26 -0800

    frontend: updated dashboard default page redirection

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default async function DashboardPage() {
-  //TODO: redirect to /dashboard/enrollment
-  return (
-    <div className="flex flex-col px-8 py-6 min-h-screen bg-gray-100 dark:bg-gray-900">
-      Dashboard content
-    </div>
-  );
+  redirect('/dashboard/enrollment');
 }
