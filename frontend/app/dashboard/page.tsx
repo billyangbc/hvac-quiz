d8e5ae9d9c47d756c9252700a00a775e4653b879
Bill Yang <bill.yang@live.ca>
2025-03-13 11:57:01 -0700

    frontend: implemented quick start page

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,5 +1,13 @@
-import { redirect } from 'next/navigation';
+import MarkdownCard from "@/components/markdown/MarkdownCard";
+import { loadFile } from "@/lib/loader";
 
+const staticFilePath: string = "/data/static";
 export default async function DashboardPage() {
-  redirect('/dashboard/enrollment');
+  const content = await loadFile(staticFilePath + "/" +  "quick-start.md");
+  return (
+    <>
+    <MarkdownCard content={content} css="border-2">
+    </MarkdownCard>
+    </>
+  );
 }
